dd3d8457deee9288adf06c33b032a3dc27d9bdfd
Joell Jones <25757151+joelljones@users.noreply.github.com>
2024-02-14 12:43:44 -0500

    feat: create basic table

---
+++ src/app/loads/page.jsx
@@ -0,0 +1,29 @@
+// (server component) is where we'll fetch data and render our table
+
+import { columns } from './columns';
+// import { DataTable } from './data-table';
+import DataTable from './data-table';
+
+// async function getData(): Promise<Payment[]> {
+async function getData() {
+  // Fetch data from your API here.
+  return [
+    {
+      id: '728ed52f',
+      amount: 100,
+      status: 'pending',
+      email: 'm@example.com',
+    },
+    // ...
+  ];
+}
+
+export default async function DemoPage() {
+  const data = await getData();
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
+}
